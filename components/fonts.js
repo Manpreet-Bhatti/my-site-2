a9512e5bca26ac20a94c398a0d8331d4b9207feb
Manpreet Bhatti <manpreet@bhatti.net>
2022-08-15 16:53:50 -0400

    Add global font

---
+++ components/fonts.js
@@ -0,0 +1,7 @@
+const Fonts = () => {
+  <style jsx global>{`
+    @import url("https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;700&display=swap");
+  `}</style>;
+};
+
+export default Fonts;
