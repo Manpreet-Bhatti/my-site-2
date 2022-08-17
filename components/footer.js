3f4f2c90836d0eb6b323dc0651255a2385b7c407
Manpreet Bhatti <manpreet@bhatti.net>
2022-08-16 23:50:33 -0400

    Finished creating footer

---
+++ components/footer.js
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+
+const Footer = () => {
+  return (
+    <Box align="center" opacity={0.4} fontSize="sm">
+      Made with ❤️ by Manpreet Bhatti.
+    </Box>
+  );
+};
+
+export default Footer;
