bbe7df1e7526c77ccd6c06addbda8b1c67560090
Manpreet Bhatti <manpreet@bhatti.net>
2022-08-17 00:36:05 -0400

    Update font to Roboto

--- components/fonts.js
+++ components/fonts.js
@@ -1,6 +1,6 @@
 const Fonts = () => {
   <style jsx global>{`
-    @import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;700&display=swap");
+    @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap");
   `}</style>;
 };
 
