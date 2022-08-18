31a0116fe7ff9ec3ba83bb3e674b56745ff42a22
Manpreet Bhatti <manpreet@bhatti.net>
2022-08-18 01:11:24 -0400

    Update "Return to home" button styles

--- pages/404.js
+++ pages/404.js
@@ -5,18 +5,25 @@ import {
   Text,
   Container,
   Divider,
+  useColorModeValue,
   Button,
 } from "@chakra-ui/react";
+import theme from "../lib/theme";
 
 const NotFound = () => {
   return (
     <Container>
       <Heading as="h1">Uh oh 😬</Heading>
-      <Text>The page you&apos;re looking for was not found. Please try another time!</Text>
+      <Text>
+        The page you&apos;re looking for was not found. Please try another time!
+      </Text>
       <Divider my={6} />
       <Box my={6} align="center">
         <NextLink href="/" passHref>
-          <Button bg="#CE2029" _hover={{ bg: "#7D1D22" }}>
+          <Button
+            bg={useColorModeValue("#8f8f8f1a", theme.colors.redLight)}
+            _hover={{ bg: useColorModeValue("#FAE8E9", "#7D1D22") }}
+          >
             Return to home
           </Button>
         </NextLink>
