9ed60fd0b8cf2286e1d500530bbe0ad73d2f5b13
Manpreet Bhatti <manpreet@bhatti.net>
2022-08-29 21:05:51 -0400

    Create single-column layout of site

---
+++ components/layouts/home.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import NavBar from "../navbar";
+import { Box, Container } from "@chakra-ui/react";
+import Footer from "../footer";
+
+const Home = ({ children, router }) => {
+  return (
+    <Box as="main" pb={8}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Manpreet's homepage" />
+        <meta name="author" content="Manpreet Bhatti" />
+        <link rel="apple-touch-icon" href="MB-mono.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <meta property="og:site_name" content="Manpreet Bhatti" />
+        <meta name="og:title" content="Manpreet Bhatti" />
+        <meta property="og:type" content="website" />
+        <title>Manpreet Bhatti - Home</title>
+      </Head>
+      <NavBar path={router.asPath} />
+      <Container maxW="container.md" pt={14}>
+        {children}
+        <Footer />
+      </Container>
+    </Box>
+  );
+};
+
+export default Home;
