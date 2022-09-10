c8c6e1a9bb6a9b397c08e1f71b5e6d19dd407488
Manpreet Bhatti <manpreet@bhatti.net>
2022-09-10 01:36:57 -0400

    Update footer

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import { Box } from "@chakra-ui/react";
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-      Made with ❤️ by Manpreet Bhatti.
+      Made with ❤️ by Manpreet Bhatti
     </Box>
   );
 };
