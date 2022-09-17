27235811abc2592ee6aaf36781ee1734f813d2e8
Manpreet Bhatti <manpreet@bhatti.net>
2022-09-17 00:39:02 -0400

    Finished setting up Graf-Gas project page

---
+++ pages/projects/grafgas.js
@@ -0,0 +1,84 @@
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { Badge, Container, Link, List, ListItem } from "@chakra-ui/react";
+import Layout from "../../components/layouts/single-column";
+import P from "../../components/paragraph";
+import { Meta, Title, WorkImage } from "../../components/work";
+
+const GrafGas = () => (
+  <Layout title="Graf-Gas">
+    <Container>
+      <Title>
+        Front End Lead <Badge>Oct 2021 - Present</Badge>
+      </Title>
+      <P>
+        A data analytical dashboard with a focus on fleet driver fuel
+        consumption and transportation efficiency.
+      </P>
+      <P>
+        I built a majority of the front end used on a daily basis by closely
+        working clients to adhere to requested features, creating transparency
+        for users so that metrics are shown accurately.
+      </P>
+      <P>
+        In doing so, I entirely automated Peru gas stations&apos; data entry
+        operations, migrating from an Excel-based to a SaaS application. Data is
+        now collected, processed, and stored on the cloud, and displayed in a
+        table and KPIs. In addition, customers can get an excel report of their
+        data for further research.
+      </P>
+      <P>
+        To achieve such feats, I worked with fellow developers to implement a
+        rigorous in-house DevOps environment and created a CI/CD pipeline to
+        deploy code to micro-services hosted on AWS.
+      </P>
+      <List ml={4} my={4}>
+        <ListItem>
+          <Meta>Website</Meta>
+          <Link variant="red-link" href="https://www.grafgas.com/login">
+            https://www.grafgas.com/login <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Meta>Platform</Meta>
+          <span>Web</span>
+        </ListItem>
+        <ListItem>
+          <Meta>Stack</Meta>
+          <span>React, Django, AWS, PostgreSQL</span>
+        </ListItem>
+      </List>
+      <WorkImage src="/images/projects/grafgas_01.png" alt="Graf-Gas login" />
+      <WorkImage
+        src="/images/projects/grafgas_02.png"
+        alt="Graf-Gas company info"
+      />
+      <WorkImage
+        src="/images/projects/grafgas_03.png"
+        alt="Graf-Gas front page"
+      />
+      <WorkImage
+        src="/images/projects/grafgas_04.png"
+        alt="Graf-Gas profile page"
+      />
+      <WorkImage
+        src="/images/projects/grafgas_05.png"
+        alt="Graf-Gas profile page in edit mode"
+      />
+      <WorkImage
+        src="/images/projects/grafgas_06.png"
+        alt="Graf-Gas fuel consumption table"
+      />
+      <WorkImage
+        src="/images/projects/grafgas_07.png"
+        alt="Graf-Gas fuel consumption KPIs"
+      />
+      <WorkImage
+        src="/images/projects/grafgas_08.png"
+        alt="Graf-Gas fuel consumption KPIs continued"
+      />
+    </Container>
+  </Layout>
+);
+
+export default GrafGas;
+export { getServerSideProps } from "../../components/chakra";
