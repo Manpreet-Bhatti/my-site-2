0d5cdd0a5badd21feeb83660746acec043115cde
Manpreet-Bhatti <manpreet@bhatti.net>
2023-04-20 18:34:11 -0400

    Update width and height to numeric value

--- pages/index.js
+++ pages/index.js
@@ -74,8 +74,8 @@ const Home = () => (
               src="/images/manpreet-coding.png"
               alt="Profile image"
               borderRadius="full"
-              width="100%"
-              height="100%"
+              width="96"
+              height="96"
             />
           </Box>
         </Box>
