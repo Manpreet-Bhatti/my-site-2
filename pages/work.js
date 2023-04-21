327999fbac883d50e6423bd4a364696aebe6eed2
Manpreet-Bhatti <manpreet@bhatti.net>
2023-04-21 00:15:52 -0400

    Add Agentnoon item

--- pages/work.js
+++ pages/work.js
@@ -10,6 +10,7 @@ import { DynamicGridItem } from "../components/grid-item";
 import Layout from "../components/layouts/single-column";
 import Section from "../components/section";
 
+import thumbAgentnoon from "../public/images/work/agentnoon.jpg";
 import thumbCriticalMass from "../public/images/work/critical-mass.jpg";
 import thumbZonado from "../public/images/work/zonado.jpg";
 import thumbIvedha from "../public/images/work/ivedha.jpg";
@@ -35,36 +36,28 @@ const Work = () => (
       </Heading>
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
         <Section>
+          <DynamicGridItem type="work" id="agentnoon" title="Agentnoon" thumbnail={thumbAgentnoon}>
+            A software company that provides Real Time Work Force Planning for Fast HR System Integrations
+          </DynamicGridItem>
+        </Section>
+        <Section>
           <DynamicGridItem
             type="work"
             id="criticalmass"
             title="Critical Mass"
             thumbnail={thumbCriticalMass}
           >
-            A full-service digital experience design agency with a relentless
-            focus on the customer
+            A full-service digital experience design agency with a relentless focus on the customer
           </DynamicGridItem>
         </Section>
-        <Section>
+        <Section delay={0.1}>
           <DynamicGridItem
             type="work"
             id="zonado"
             title="Zonado"
             thumbnail={thumbZonado}
           >
-            A specialised marketplace for Canadian commercial real estate and
-            business sales
-          </DynamicGridItem>
-        </Section>
-        <Section delay={0.1}>
-          <DynamicGridItem
-            type="work"
-            id="ivedha"
-            title="iVedha"
-            thumbnail={thumbIvedha}
-          >
-            A leading global cloud managed services provider for enterprises in
-            Canada, USA, Mexico and across the world
+            A specialised marketplace for Canadian commercial real estate and business sales
           </DynamicGridItem>
         </Section>
       </SimpleGrid>
@@ -77,12 +70,21 @@ const Work = () => (
           <Section delay={0.3}>
             <DynamicGridItem
               type="work"
+              id="ivedha"
+              title="iVedha"
+              thumbnail={thumbIvedha}
+            >
+              A leading global cloud managed services provider for enterprises in Canada, USA, Mexico and across the world
+            </DynamicGridItem>
+          </Section>
+          <Section delay={0.3}>
+            <DynamicGridItem
+              type="work"
               id="cheapreats"
               title="CheaprEats"
               thumbnail={thumbCheaprEats}
             >
-              A food pickup service that facilitates interactions between
-              customers and vendors while targeting post-secondary students
+              A food pickup service that facilitates interactions between customers and vendors while targeting post-secondary students
             </DynamicGridItem>
           </Section>
           <Section delay={0.3}>
@@ -92,8 +94,7 @@ const Work = () => (
               title="benchMarked Studios"
               thumbnail={thumbBenchMarkedStudios}
             >
-              A consulting agency that looks to solve problems with robust,
-              tested, specifically engineered software
+              A consulting agency that looks to solve problems with robust, tested, specifically engineered software
             </DynamicGridItem>
           </Section>
         </SimpleGrid>
