5a2a129ad41480ad6f31a9c881addd03302db710
Manpreet-Bhatti <manpreet@bhatti.net>
2023-08-17 14:09:59 -0400

    Turn off antialias

--- components/model.js
+++ components/model.js
@@ -47,7 +47,7 @@ const ComputerModel = () => {
       const scH = container.clientHeight;
 
       const renderer = new THREE.WebGLRenderer({
-        antialias: true,
+        antialias: false,
         alpha: true,
       });
       renderer.setPixelRatio(window.devicePixelRatio);
