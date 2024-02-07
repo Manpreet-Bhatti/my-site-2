e7823e6214abcfb5e3df71873c313040bf6c9282
Manpreet Bhatti <manpreet@bhatti.net>
2024-02-07 03:35:39 -0500

    Test

--- pages/index.js
+++ pages/index.js
@@ -12,18 +12,29 @@ import {
   useColorModeValue,
   Text,
 } from "@chakra-ui/react";
-import { Link } from "@chakra-ui/next-js";
 import { ChevronRightIcon } from "@chakra-ui/icons";
 import Paragraph from "../components/paragraph";
 import Layout from "../components/layouts/single-column";
 import Section from "../components/section";
 import { GridItem } from "../components/grid-item";
 import { IoLogoLinkedin, IoLogoGithub, IoMail } from "react-icons/io5";
-import Image from "next/image";
 import theme from "../lib/theme";
+import dynamic from "next/dynamic";
 
 import thumbMenstruationNation from "../public/images/projects/menstruation_nation/mnation_logo.png";
 import thumbGrafGas from "../public/images/projects/grafgas.jpg";
+
+const Link = dynamic(
+  () => import("@chakra-ui/next-js").then((mod) => mod.Link),
+  {
+    ssr: false,
+  }
+);
+
+const Image = dynamic(() => import("next/image"), {
+  loading: () => <p>Loading...</p>,
+  ssr: false,
+});
 
 const Home = () => (
   <Layout>
@@ -210,3 +221,4 @@ const Home = () => (
 );
 
 export default Home;
+export { getServerSideProps } from "../components/chakra";
